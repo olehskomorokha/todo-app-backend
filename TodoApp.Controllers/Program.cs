e1d0267f1e3c7c58e1fd07e3affedfdc62ba1077
olehskomorokha <oleg.skomoroha1@gmail.com>
2026-08-22 12:33:06 +0300

    correct exception handler

--- TodoApp.Controllers/Program.cs
+++ TodoApp.Controllers/Program.cs
@@ -10,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddProblemDetails();
 
 // register database
 builder.Services.AddDbContext<AppDbContext>(options =>
